0be204698018447c5cd068abd353100340609d14
Adryano Junior <jusamsung10@gmail.com>
2024-11-15 13:25:23 -0300

    testing javascript

---
+++ objetos.js
@@ -0,0 +1,8 @@
+let fruits = ['Maçã', 'Banana', 'Uva', 'Morango']
+console.log(fruits.length)
+
+let firstFruit = fruits[0]
+console.log(firstFruit)
+
+let lastFruit = fruits[2]
+console.log(lastFruit)
---
+++ variaveis.js
@@ -0,0 +1,10 @@
+var name= 'Junior'
+console.log(name)
+
+{
+  var otherName = 'César'
+    console.log(otherName)
+}
+
+const constante = 0
+console.log(constante)
